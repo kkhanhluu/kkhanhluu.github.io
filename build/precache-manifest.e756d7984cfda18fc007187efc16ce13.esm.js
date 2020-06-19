2b3ec2e05b74cf8f1b3ff4dd026b5d080270ff40
Khanh Luu <luukhanh96@gmail.com>
2020-06-19 13:37:27 +0200

    deploy again

---
+++ build/precache-manifest.e756d7984cfda18fc007187efc16ce13.esm.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "ebb70948611541c1651f98d689ded294",
+    "url": "/assets/images/avatar.jpg"
+  },
+  {
+    "revision": "b7dfb08335b6ed564022afe4dd02221b",
+    "url": "/assets/images/covid19.png"
+  },
+  {
+    "revision": "ac14fae0be8ee0fd895adef5e1f342d3",
+    "url": "/assets/images/lukaloka.png"
+  },
+  {
+    "revision": "81cb2dd70d78b2271e3713a640d0c66e",
+    "url": "/assets/images/movieapp.png"
+  },
+  {
+    "revision": "0c6221f26ce5401b7efe67745625e497",
+    "url": "/assets/images/natours.png"
+  },
+  {
+    "revision": "5f812e79730dcc3bbc4376db0da566fb",
+    "url": "/assets/images/shoppingApp.png"
+  },
+  {
+    "revision": "356ae3c64ddf364403bc",
+    "url": "/bundle.62582.css"
+  },
+  {
+    "revision": "356ae3c64ddf364403bc",
+    "url": "/bundle.6af2a.esm.js"
+  },
+  {
+    "revision": "b70e8b0b9c44c6a9ed52014acfd82ffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09446198657efe85c470",
+    "url": "/polyfills.a34c8.esm.js"
+  }
+]);
